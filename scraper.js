d07bb0b8baeb8137dbb2e643573d98c03782eaf0
LustyCodes <131748822+LustyCodes@users.noreply.github.com>
2025-02-14 23:38:27 +0600

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -46,7 +46,7 @@ const convertRuntimeToMinutes = (runtimeStr) => {
 class AdultDVDEmpireScraper {
     baseUrl;
     headers;
-    constructor(baseUrl = 'https://proxy.lustycodes.workers.dev/?url=https://www.adultempire.com') {
+    constructor(baseUrl = 'https://www.adultempire.com') {
         this.baseUrl = baseUrl;
         this.headers = {
             'User-Agent': getRandomUserAgent(),
