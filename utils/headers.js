8c1fd1f6a46aa850f7cc2bf000b7f7b4d31b5b6f
LustyCodes <131748822+LustyCodes@users.noreply.github.com>
2025-02-14 22:52:14 +0600

    Update headers.js

--- utils/headers.js
+++ utils/headers.js
@@ -1,12 +1,22 @@
 // Common headers for making requests
 const headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
     'Accept-Language': 'en-US,en;q=0.5',
     'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
-    'Cache-Control': 'max-age=0',
-    'Cookie': 'ageConfirmed=true'
+    'Cookie': 'etoken=a1=162dcea684cade07867e61860e149cc78c68f41316bdce22ea02b158c333b3c9&a2=306e75d55b3e4ee6414e66adfd41390efe637dd29c82603707d10bffcd44b4ea&a3=99475898726395; ageConfirmed=true',
+    'DNT': '1',
+    'Host': 'www.adultempire.com',
+    'Priority': 'u=0, i',
+    'Referer': 'https://www.adultempire.com/AgeConfirmation?url2=/',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Sec-GPC': '1',
+    'TE': 'trailers',
+    'Upgrade-Insecure-Requests': '1'
 };
 
 module.exports = {
