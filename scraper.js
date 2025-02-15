1658c9f98f9de0c20f97bd960460b9f0e7dd0a50
LustyCodes <131748822+LustyCodes@users.noreply.github.com>
2025-02-16 00:13:38 +0600

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -49,20 +49,7 @@ class AdultDVDEmpireScraper {
     constructor(baseUrl = 'https://adultempire.lustycodes.workers.dev/?url=https://www.adultempire.com') {
         this.baseUrl = baseUrl;
         this.headers = {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Connection": "keep-alive",
-            "DNT": "1",
-            "Host": "localhost:3000",
-            "If-None-Match": 'W/"1edd-uYSc3vlBxHYxUBlcSnf2hwsdLjQ"',
-            "Priority": "u=0, i",
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-Site": "none",
-            "Sec-Fetch-User": "?1",
-            "Sec-GPC": "1",
-            "Upgrade-Insecure-Requests": "1",
+            'Cookie': 'ageConfirmed=true',
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0"
         };
     }
